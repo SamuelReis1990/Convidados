bfb45f8d3b47c4f2ffb81b9b8985967027d6ea4f
Samuel Reis <samuelreis.1990@gmail.com>
2018-10-29 22:19:27 -0300

    Alteração do para pegar o banco no contexto

--- Convidados-MVC/Models/Contexto.cs
+++ Convidados-MVC/Models/Contexto.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 
 namespace Convidados_MVC.Models
@@ -5,8 +6,8 @@ namespace Convidados_MVC.Models
     public class Contexto : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Data Source=Casamento.db");
+        {            
+            optionsBuilder.UseSqlite("Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Casamento.db"));
         }
 
          public DbSet<Convidado> Convidado { get; set; }
